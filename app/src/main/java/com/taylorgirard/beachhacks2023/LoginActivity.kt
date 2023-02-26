c817a9b3ad59ebb67119a76e8aa06ea8edbb26ab
Taylor Girard <686taytay@gmail.com>
2023-02-25 17:57:14 -0800

    Created basic login page

--- app/src/main/java/com/taylorgirard/beachhacks2023/LoginActivity.kt
+++ app/src/main/java/com/taylorgirard/beachhacks2023/LoginActivity.kt
@@ -2,10 +2,25 @@ package com.taylorgirard.beachhacks2023
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class LoginActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        var btnLogin = findViewById<Button>(R.id.btnLogin)
+        var etEmail = findViewById<EditText>(R.id.etEmail)
+        var etPassword = findViewById<EditText>(R.id.etPassword)
+
+        btnLogin.setOnClickListener {
+            val myToast = Toast.makeText(applicationContext, "Hello Toast!", Toast.LENGTH_SHORT)
+            myToast.show()
+        }
     }
+
 }
