5126967083eced8c4d46dd234dd13efad9fda709
Taylor Girard <686taytay@gmail.com>
2023-02-25 22:02:18 -0800

    Fixed bug with moving map camera to user location

--- app/src/main/java/com/taylorgirard/beachhacks2023/MainActivity.kt
+++ app/src/main/java/com/taylorgirard/beachhacks2023/MainActivity.kt
@@ -4,9 +4,9 @@ import android.Manifest
 import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.location.Location
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.google.android.gms.location.FusedLocationProviderClient
@@ -16,10 +16,10 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.tasks.Task
 
 private var locationPermissionGranted = false
-private var lastKnownLocation: Location? = null
+//private var lastKnownLocation: Location? = null
 private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
@@ -45,74 +45,50 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+    @SuppressLint("MissingPermission")
     override fun onMapReady(googleMap: GoogleMap) {
         this.map = googleMap
-
-        updateLocationUI()
-        getDeviceLocation()
-
-    }
-
-    @SuppressLint("MissingPermission")
-    private fun updateLocationUI() {
-        if (map == null) {
-            return
-        }
-        try {
-            if (locationPermissionGranted) {
-                map?.isMyLocationEnabled = true
-                map?.uiSettings?.isMyLocationButtonEnabled = true
-            } else {
-                map?.isMyLocationEnabled = false
-                map?.uiSettings?.isMyLocationButtonEnabled = false
-                lastKnownLocation = null
-                getLocationPermission()
-            }
-        } catch (e: SecurityException) {
-            Log.e("Exception: %s", e.message, e)
-        }
-    }
-
-    @SuppressLint("MissingPermission")
-    private fun getDeviceLocation() {
-        /*
-         * Get the best and most recent location of the device, which may be null in rare
-         * cases when a location is not available.
-         */
-        try {
-            if (locationPermissionGranted) {
-                val locationResult = fusedLocationProviderClient.lastLocation
-                locationResult.addOnCompleteListener(this) { task ->
-                    if (task.isSuccessful) {
-                        // Set the map's camera position to the current location of the device.
-                        lastKnownLocation = task.result
-                        if (lastKnownLocation != null) {
-                            map?.moveCamera(CameraUpdateFactory.newLatLngZoom(
-                                LatLng(lastKnownLocation!!.latitude,
-                                    lastKnownLocation!!.longitude), 15.toFloat()))
-                        }
-                    }
-                }
-            }
-        } catch (e: SecurityException) {
-            Log.e("Exception: %s", e.message, e)
-        }
-    }
-
-    private fun getLocationPermission() {
-        /*
-         * Request location permission, so that we can get the location of the
-         * device. The result of the permission request is handled by a callback,
-         * onRequestPermissionsResult.
-         */
         if (ContextCompat.checkSelfPermission(this.applicationContext,
                 Manifest.permission.ACCESS_FINE_LOCATION)
-            == PackageManager.PERMISSION_GRANTED) {
-            locationPermissionGranted = true
-        } else {
+            != PackageManager.PERMISSION_GRANTED){
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),1)
         }
+        val locationResult = fusedLocationProviderClient.lastLocation
+        locationResult.addOnCompleteListener(this) { task ->
+            if (task.isSuccessful) {
+                // Set the map's camera position to the current location of the device.
+                val lastKnownLocation = task.result
+                if (lastKnownLocation != null) {
+                    map?.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                        LatLng(lastKnownLocation!!.latitude,
+                            lastKnownLocation!!.longitude), 15.toFloat()))
+                }
+            }
+        }
     }
+
+    @SuppressLint("MissingPermission")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        val locationResult = fusedLocationProviderClient.lastLocation
+        locationResult.addOnCompleteListener(this) { task ->
+            if (task.isSuccessful) {
+                // Set the map's camera position to the current location of the device.
+                val lastKnownLocation = task.result
+                if (lastKnownLocation != null) {
+                    map?.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                        LatLng(lastKnownLocation!!.latitude,
+                            lastKnownLocation!!.longitude), 15.toFloat()))
+                }
+            }
+        }
+    }
+
+
 
 
 }
--- app/src/main/java/com/taylorgirard/beachhacks2023/SignUpActivity.kt
+++ app/src/main/java/com/taylorgirard/beachhacks2023/SignUpActivity.kt
@@ -1,34 +1,26 @@
 package com.taylorgirard.beachhacks2023
 
-import android.Manifest
 import android.content.Intent
-import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 private lateinit var auth: FirebaseAuth
-val TAG = "LOGIN ACTIVITY"
 
-class LoginActivity : AppCompatActivity() {
-
+class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_sign_up)
 
-        var btnLogin = findViewById<Button>(R.id.btnLogin)
-        var btnSignUp = findViewById<Button>(R.id.btnSignUp)
-        var etEmail = findViewById<EditText>(R.id.etEmail)
-        var etPassword = findViewById<EditText>(R.id.etPassword)
+        var btnSignUp = findViewById<Button>(R.id.SignUpButton)
+        var etEmail = findViewById<EditText>(R.id.SignUpEmail)
+        var etPassword = findViewById<EditText>(R.id.SignUpPassword)
 
         val user = Firebase.auth.currentUser
         if (user != null) {
@@ -38,34 +30,28 @@ class LoginActivity : AppCompatActivity() {
         }
 
         btnSignUp.setOnClickListener {
-            val intent = Intent(this, SignUpActivity::class.java)
-            startActivity(intent)
-        }
-
-        btnLogin.setOnClickListener {
             val email = etEmail.text.toString()
             val password = etPassword.text.toString()
 
             auth = Firebase.auth
 
-            auth.signInWithEmailAndPassword(email, password)
+            auth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this) { task ->
                     if (task.isSuccessful) {
                         // Sign in success, update UI with the signed-in user's information
-                        Log.d(TAG, "signInWithEmail:success")
+                        Log.d(TAG, "createUserWithEmail:success")
+                        val user = auth.currentUser
                         val intent = Intent(this, MainActivity::class.java)
                         startActivity(intent)
                         finish()
                     } else {
                         // If sign in fails, display a message to the user.
-                        Log.w(TAG, "signInWithEmail:failure", task.exception)
+                        Log.w(TAG, "createUserWithEmail:failure", task.exception)
                         Toast.makeText(baseContext, "Authentication failed.",
                             Toast.LENGTH_SHORT).show()
+
                     }
                 }
         }
     }
-
-
-
 }
